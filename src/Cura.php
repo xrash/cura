92735d2db1a02e11223e69af847c15196b50a7f6
Felipe Gonçalves <felipe@xra.sh>
2014-12-24 09:05:49 -0200

    Work in progress.

--- src/Cura.php
+++ src/Cura.php
@@ -2,6 +2,38 @@
 
 class Cura
 {
+
+    public static function dump($value)
+    {
+        $type = gettype($value);
+
+        $info = array();
+
+        switch ($type) {
+            case 'array':
+                $info[] = sprintf('type: array (%d)', count($value));
+                $info[] = sprintf('value: %s', var_export($value, true));
+                break;
+
+            case 'object':
+                $info[] = sprintf('type: object (%s)', get_class($value));
+                $info[] = sprintf('value: %s', var_export($value, true));
+                break;
+
+            case 'resource':
+                $info[] = sprintf('type: resource (%s)', get_resource_type($value));
+                $info[] = sprintf('value: %s', var_export($value, true));
+                break;
+
+            default:
+                $info[] = sprintf('type: %s', $type);
+                $info[] = sprintf('value: %s', $value);
+                break;
+        }
+
+        return sprintf("\n%s\n", implode("\n", $info));
+    }
+
     public static function backtrace($trace)
     {
         if (!$trace) {
@@ -42,18 +74,16 @@ class Cura
         $args = array();
 
         foreach ($frame['args'] as $arg) {
-            switch (true) {
-                case is_int($arg):
-                case is_string($arg):
-                    $args[] = $arg;
-                    break;
-                case is_array($arg):
+            $type = gettype($arg);
+
+            switch ($type) {
+                case 'array':
                     $args[] = sprintf('array#%d', count($arg));
                     break;
-                case is_object($arg):
+                case 'object':
                     $args[] = sprintf('object#%s', get_class($arg));
                     break;
-                case is_resource($arg):
+                case 'resource':
                     $args[] = sprintf('resource#%s', get_resource_type($arg));
                     break;
                 default:
--- src/shortcuts.php
+++ src/shortcuts.php
@@ -1,24 +1,50 @@
 <?php
 
-function b($trace)
+function d($value)
+{
+    echo Cura::dump($value);
+}
+
+function de($value)
+{
+    echo Cura::dump($value);
+    exit;
+}
+
+function wd($value)
+{
+    echo '<pre>';
+    echo Cura::dump($value);
+    echo '</pre>';
+}
+
+function wde($value)
+{
+    echo '<pre>';
+    echo Cura::dump($value);
+    echo '</pre>';
+    exit;
+}
+
+function b($trace = null)
 {
     echo Cura::backtrace($trace);
 }
 
-function wb($trace)
+function wb($trace = null)
 {
     echo '<pre>';
     echo Cura::backtrace($trace);
     echo '</pre>';
 }
 
-function be($trace)
+function be($trace = null)
 {
     echo Cura::backtrace($trace);
     exit;
 }
 
-function wbe($trace)
+function wbe($trace = null)
 {
     echo '<pre>';
     echo Cura::backtrace($trace);
